235a9f83284bc389a7a480ed5ce579a6890efc31
João Pereira <joaopereira.eng.tec@gmail.com>
2020-12-01 18:15:39 +0000

    Adds Phrase's Repository and function getPhrase

--- app/src/main/java/com/coding/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/coding/motivation/ui/MainActivity.kt
@@ -7,10 +7,12 @@ import com.coding.motivation.R
 import com.coding.motivation.databinding.ActivityMainBinding
 import com.coding.motivation.infra.MotivationConstants
 import com.coding.motivation.infra.SecurityPreferences
+import com.coding.motivation.repository.Mock
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mSecurityPreferences: SecurityPreferences
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var mPhraseFilter: Int = MotivationConstants.PHRASEFILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,9 @@ class MainActivity : AppCompatActivity() {
 
         mSecurityPreferences = SecurityPreferences(this)
         binding.textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+        binding.imageAll.setColorFilter(resources.getColor(R.color.teal_200)) //Lógica inicial de seleção
+        handleNewPhrase()
 
         val listFilter = listOf(binding.imageAll.id, binding.imageHappy.id, binding.imageMorning.id)
 
@@ -42,7 +47,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleNewPhrase() {
-
+        binding.textPhrase.text = Mock().getPhrase(mPhraseFilter)
     }
 
     private fun handleFilter(id: Int) {
@@ -53,12 +58,15 @@ class MainActivity : AppCompatActivity() {
         when (id) {
             binding.imageAll.id -> {
                 binding.imageAll.setColorFilter(resources.getColor(R.color.teal_200))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.ALL
             }
             binding.imageHappy.id -> {
                 binding.imageHappy.setColorFilter(resources.getColor(R.color.teal_200))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.HAPPY
             }
             binding.imageMorning.id -> {
                 binding.imageMorning.setColorFilter(resources.getColor(R.color.teal_200))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.MORNING
             }
         }
     }
